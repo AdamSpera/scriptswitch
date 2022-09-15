d365c99eb41d32d3da08510777a6c1e05aa6e29e
AdamSpera <adamspera@hotmail.com>
2022-09-15 16:04:51 -0400

    Update code organization

--- ScriptGUI.py
+++ ScriptGUI.py
@@ -27,13 +27,13 @@ def restartOption():
         inquirer.List(
             "restartChoice",
             message="Script deployed successfully. Would you like to send another script?",
-            choices=["Send another script", "Exit the program"],
+            choices=["Script more!", "Exit the program."],
         ),
     ]
     answers = inquirer.prompt(questions)
-    if answers["restartChoice"] == "Send another script":
+    if answers["restartChoice"] == "Script more!":
         optionsMenu()
-    if answers["restartChoice"] == "Exit the program":
+    if answers["restartChoice"] == "Exit the program.":
         sys.exit("Goodbye! Thanks for using ScriptSwitch!")
 
 
@@ -41,8 +41,9 @@ def optionsMenu():
     questions = [
         inquirer.List(
             "initialChoice",
-            message="Would you like to select a script or create a new one?",
-            choices=["Create a new script", "Use a template script"],
+            message="Would you like to select a script , create a new one, or configure an interface?",
+            choices=["Create a new script",
+                     "Use a template script", "Configure an interface"],
         ),
     ]
     answers = inquirer.prompt(questions)
@@ -59,48 +60,52 @@ def optionsMenu():
 
         restartOption()
 
+    if answers["initialChoice"] == "Configure an interface":
+        output = net_connect.send_command(
+            "show ip interface brief", use_textfsm=True)
+
+        optionsList = []
+        for i in output:
+            optionsList.append(i["intf"])
+
+        questions = [
+            inquirer.List(
+                "templateChoice",
+                message="Select an interface to configure",
+                choices=optionsList,
+            ),
+        ]
+        interfaceAnswer = inquirer.prompt(questions)
+
+        questions = [
+            inquirer.List(
+                "templateChoice",
+                message="Select a configuration action for the interface",
+                choices=["Set as trunk port", "Set as access port",
+                         "Set IP address", "Set port shutdown", "Set port no shutdown"],
+            ),
+        ]
+        configureAnswer = inquirer.prompt(questions)
+
+        # Add configure port functionality here
+
+        restartOption()
+
     if answers["initialChoice"] == "Use a template script":
         questions = [
             inquirer.List(
                 "templateChoice",
                 message="Select a template script to use",
                 choices=["copy running-config startup-config", "show cdp neighbors", "show interface status",
-                         "show interface summary", "show interfaces switchport", "show mac address-table", "show running-config", "CONFIGURE AN INTERFACE"],
+                         "show interface summary", "show interfaces switchport", "show mac address-table", "show running-config"],
             ),
         ]
         answers = inquirer.prompt(questions)
 
-        if answers["templateChoice"] != "CONFIGURE AN INTERFACE":
-            print("# SWITCH OUTPUT BELOW \/ ----------------------------------")
-            output = net_connect.send_command(answers["templateChoice"])
-            print(output)
-            print("\n# SWITCH OUTPUT ABOVE /\\ ----------------------------------\n")
-            with open('Output.txt', 'w') as f:
-                f.write(output)
-        else:
-            output = net_connect.send_command(
-                "show ip interface brief", use_textfsm=True)
-
-            optionsList = []
-            for i in output:
-                optionsList.append(i["intf"])
-            questions = [
-                inquirer.List(
-                    "templateChoice",
-                    message="Select an interface to configure",
-                    choices=optionsList,
-                ),
-            ]
-            interfaceAnswer = inquirer.prompt(questions)
-
-            questions = [
-                inquirer.List(
-                    "templateChoice",
-                    message="Select a configuration option for the interface",
-                    choices=["Configure as a trunk port", "Configure as an access port", "Set IP address", "Set port shutdown", "Set port no shutdown"],
-                ),
-            ]
-            interfaceAnswer = inquirer.prompt(questions)
+        print("# SWITCH OUTPUT BELOW \/ ----------------------------------")
+        output = net_connect.send_command(answers["templateChoice"])
+        print(output)
+        print("\n# SWITCH OUTPUT ABOVE /\\ ----------------------------------\n")
 
         restartOption()
 
